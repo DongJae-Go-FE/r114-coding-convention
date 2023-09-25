1404df89ddf3798673c8b55291ee2ae411f5c752
kdj <masterforce999@naver.com>
2023-09-26 08:51:57 +0900

    router setting end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Link to="/">링크 셋팅</Link>
+      <Routes>
+        <Route path="/">라우터 셋팅 화면</Route>
+        {/* <Route path="/" element={<Layout />}></Route> */}
+      </Routes>
     </div>
   );
 }
