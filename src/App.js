d8a86b8073a8faf0582d2c44dd912a489118f31e
kdj <masterforce999@naver.com>
2023-10-05 14:34:41 +0900

    router end

--- src/App.js
+++ src/App.js
@@ -15,7 +15,23 @@ function App() {
         <LeftMenu />
         <Content>
           <Routes>
-            <Route path="/" element={<>라우터 셋팅 화면</>} />
+            <Route path="/" element={<>라우터 셋팅 화면 메인</>} />
+            <Route path="/0" element={<>라우터 셋팅 화면 0-1</>} />
+            <Route path="/0/2" element={<>라우터 셋팅 화면 0-2</>} />
+            <Route path="/0/3" element={<>라우터 셋팅 화면 0-3</>} />
+            <Route path="/0/4" element={<>라우터 셋팅 화면 0-4</>} />
+            <Route path="/1" element={<>라우터 셋팅 화면 1-1</>} />
+            <Route path="/1/2" element={<>라우터 셋팅 화면 1-2</>} />
+            <Route path="/1/3" element={<>라우터 셋팅 화면 1-3</>} />
+            <Route path="/1/4" element={<>라우터 셋팅 화면 1-4</>} />
+            <Route path="/2" element={<>라우터 셋팅 화면 2-1</>} />
+            <Route path="/2/2" element={<>라우터 셋팅 화면 2-2</>} />
+            <Route path="/2/3" element={<>라우터 셋팅 화면 2-3</>} />
+            <Route path="/2/4" element={<>라우터 셋팅 화면 2-4</>} />
+            <Route path="/3" element={<>라우터 셋팅 화면 3-1</>} />
+            <Route path="/3/2" element={<>라우터 셋팅 화면 3-2</>} />
+            <Route path="/3/3" element={<>라우터 셋팅 화면 3-3</>} />
+            <Route path="/3/4" element={<>라우터 셋팅 화면 3-4</>} />
           </Routes>
           <Footer />
         </Content>
--- src/components/organism/Header/index.js
+++ src/components/organism/Header/index.js
@@ -19,10 +19,10 @@ function Header() {
   let routerState = useSelector((state) => state.router);
 
   const headerMenuData = [
-    { key: "0", title: "링크1" },
-    { key: "1", title: "링크2" },
-    { key: "2", title: "링크3" },
-    { key: "3", title: "링크4" },
+    { key: "0", title: "링크0" },
+    { key: "1", title: "링크1" },
+    { key: "2", title: "링크2" },
+    { key: "3", title: "링크3" },
   ];
 
   return (
