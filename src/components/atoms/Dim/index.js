0104bc11d7ec4799e7db5e8244145217857a81ce
kdj <masterforce999@naver.com>
2023-10-08 03:01:14 +0900

    dim exception handling add

--- src/components/atoms/Dim/index.js
+++ src/components/atoms/Dim/index.js
@@ -4,20 +4,21 @@ import { drawerStateChange } from "../../../store/store";
 import Styled from "./styled";
 
 function Dim({ children }) {
+  const body = document.body;
   let dispatch = useDispatch();
 
   useEffect(() => {
-    document.body.style.cssText = `
+    body.style.cssText = `
       position: fixed; 
       top: -${window.scrollY}px;
       overflow-y: auto;
       width: 100%;`;
     return () => {
       const scrollY = document.body.style.top;
-      document.body.style.cssText = "";
+      body.style.cssText = "";
       window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     };
-  }, []);
+  }, [body]);
 
   return (
     <Styled
