bdeb124922c611554cfaee2e1bfa6f7d13e90366
kdj <masterforce999@naver.com>
2023-10-09 18:51:08 +0900

    data update

--- src/components/organism/Header/MenuData.js
+++ src/components/organism/Header/MenuData.js
@@ -1,8 +1,8 @@
 let MenuData = [
-  { key: "0", title: "링크0" },
-  { key: "1", title: "링크1" },
-  { key: "2", title: "링크2" },
-  { key: "3", title: "링크3" },
+  { key: "0", title: "기본 정책" },
+  { key: "1", title: "규칙 가이드" },
+  { key: "2", title: "라이브러리" },
+  { key: "3", title: "웹 접근성" },
 ];
 
 export { MenuData };
